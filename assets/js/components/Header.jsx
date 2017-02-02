52057321b9dbed0869f619b75f48b43bd91d3adc
Jose Arenado <vidalstat@gmail.com>
2017-02-02 14:27:33 -0500

    forgot to add files

---
+++ assets/js/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Title from "./Header/Title";
+
+class Header extends React.Component {
+  render() {
+    return (
+	<Title />
+    );
+  }
+}
+
+export default Header;
